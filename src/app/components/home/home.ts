fef43cb760b73c4955cc8b7a6c0ff65ac4750244
zcodlab <zmamanir@uni.edu.pe>
2026-09-23 11:21:57 -0500

    add divs to class slider sin scss

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -8,4 +8,13 @@ import { Footer } from '../footer/footer';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  // Definimos las diapositivas con tipos de contenido específicos
+  slides = [
+    { url: 'assets/imagenes/slider/bg1.jpg', type: 'hero' },
+    { url: 'assets/imagenes/slider/bg2.jpg', type: 'about' },
+    { url: 'assets/imagenes/slider/bg3.jpg', type: 'easy' },
+    { url: 'assets/imagenes/slider/bg4.jpg', type: 'problems' }
+  ];
+  currentIndex = 0;
+}
